c54ceb11b5175430267e03cd8d925e4598971e2b
catdevdev <catdev.dev@gmail.com>
2021-02-06 21:07:22 +0200

    started layout of assignment_1

--- client/pages/assignment1/index.tsx
+++ client/pages/assignment1/index.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+/* UI */
+import { Container } from "reactstrap";
+import { ListGroup, ListGroupItem, Input, Button, Row, Col } from "reactstrap";
 
 const Assigment1 = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Container>
+        <ListGroup>
+          <ListGroupItem>
+            <p>Input:</p>
+            <Row>
+              <Col>
+                <ListGroupItem>
+                  <p>x0</p>
+                  <Input></Input>
+                </ListGroupItem>
+              </Col>
+              <Col>
+                <ListGroupItem>
+                  <p>h1</p>
+                  <Input></Input>
+                </ListGroupItem>
+              </Col>
+            </Row>
+            <ListGroupItem style={{marginTop: 18}} >
+              <Button style={{ width: "100%" }} color="primary">
+                Calculate
+              </Button>
+            </ListGroupItem>
+          </ListGroupItem>
+          <ListGroupItem>
+            <p>Results:</p>
+            <Input></Input>
+          </ListGroupItem>
+        </ListGroup>
+      </Container>
+    </div>
+  );
 };
 
 export default Assigment1;
