37873faef9ac1d0cd7bae073e250d480de78cb03
catdevdev <catdev.dev@gmail.com>
2021-02-06 18:17:23 +0200

    installed autopep8

--- server/main.py
+++ server/main.py
@@ -1,21 +1,30 @@
-""" imports """
+# imports
 from flask import Flask
-from flask_restful import Api, Resource
-""" math imports """
+from flask_restful import Api, Resource, reqparse
+# math imports
 from sympy import *
+
 
 app = Flask(__name__)
 api = Api(app)
 
+math_put_args = reqparse.RequestParser()
+math_put_args.add_argument("x", type=float, help="for x variable")
+math_put_args.add_argument("h", type=float, help="for h variable")
+
+x = Symbol('x')
+
 class Test1(Resource):
-    def get(self):
-        x = Symbol('x')
-        f = x**2
-        f_prime = f.diff(x)
-        print(f_prime.subs(x, 10))
-        return {"test": int(f_prime.subs(x, 10))}
-
-
+    def post(self):
+        
+        def expression():            
+            return x**2**x
+        def takeFirstDerivities():            
+            f = expression()
+            return f.diff(x)
+        def calculateFirstDerivities(x_value):    
+            return float(takeFirstDerivities().subs(x, x_value))
+        return {"value": calculateFirstDerivities(math_put_args.parse_args().x)}
 api.add_resource(Test1, "/")
 
 
