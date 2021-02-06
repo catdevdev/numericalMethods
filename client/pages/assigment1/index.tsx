dabcdfa278dbe0500e93fae06455686f08833baf
catdevdev <catdev.dev@gmail.com>
2021-02-06 19:15:18 +0200

    refactor client

---
+++ client/pages/assigment1/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Assigment1 = () => {
+  return <div></div>;
+};
+
+export default Assigment1;
---
+++ client/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Button, Container } from "reactstrap";
+
+const Index = () => {
+  return (
+    <div>
+      <Container>
+        
+      </Container>
+    </div>
+  );
+};
+
+export default Index;
